c66b391e3bfe90f13e8de933332d94f25ddf3b1f
Nigel Nicholas <nicholasnigel@yahoo.com>
2018-03-30 01:27:46 +0800

    fixed param.pl

--- param.pl
+++ param.pl
@@ -9,17 +9,20 @@ sub getname {
     my $n = scalar(@_);
 
     print "Number of parameters passed are ". ${n}. "\n";
-    my i = 0;
-    foreach $name (@_) {
-        my $namelist[i] = shift @_;
-        i++;
+    my $i = 0;
+    my @namelist = (' ');
+    foreach my $item (@_) {
+         $namelist[$i] = $item;
+        $i++;
     }
-    foreach @names (@namelist) {
+    foreach my $names (@namelist) {
         print ${names} . "\n";
     }
-
+    print "first person in the name list is ". ${namelist[0]} . "\n";
 }
 
 
 # main function
 getname('fred','zack','m-god');
+print "next one is \n";
+getname('joseph','mikey');

512621d680c4cc5c27da7b059cadf1d5ee8e108d
Nigel Nicholas <nicholasnigel@yahoo.com>
2018-03-31 23:46:15 +0800

    intermediate

--- assignment3_v2/task1/Game.pm
+++ assignment3_v2/task1/Game.pm
@@ -23,11 +23,20 @@ sub set_players {
     #$player_list to record the number of players
     my $player_list = shift \@_;
     my $player_number = scalar(@{$player_list});
-    
+
+    my $index = 0;
+    #for each name in the player_list, initialize the 
+    foreach(@{$player_list}) {
+        $self->{"players"}[$i] = Player->new($_);
+        $index++
+    }
+    return 1; # to show that creation is successful, error message should be in the start_game
 }
 
+#calculate how many will be returned to the player from the current stack
 sub getReturn {
-
+    #check in the list if there is a repetiiton traverse the list
+    $
 }
 
 sub showCards {

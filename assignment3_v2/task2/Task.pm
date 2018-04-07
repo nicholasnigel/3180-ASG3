4d02619e58bd8ad93d41640e77296a2557a78df5
Nigel Nicholas <nicholasnigel@yahoo.com>
2018-04-07 15:02:20 +0800

    finished task.pm

--- assignment3_v2/task2/Task.pm
+++ assignment3_v2/task2/Task.pm
@@ -8,15 +8,41 @@ our $pid = 0 ;       # the first pid ever should be 0
 sub new {   # calling new = Task->new(name,time)
     my $class = shift @_;   
     my $name = shift @_;    
-    my $time = shift @_;
+    my $total_time = shift @_;
 
     my $object = bless {
         "name" => $name,
-        "time" => $time,
+        "total_time" => $total_time,
         "pid" => $pid,
     }, $class;
 
     return $object;
+}
+
+#```````````````SUBROUTINE TO GET OBJECT NAME ```````````````````
+sub getName {       # sample use: $task->getName();
+    my $self = shift @_;
+    
+    my $name_return = $self->{"name"};
+
+    return $name_return;
+}
+
+#`````````````````SUBROUTINE TO GET THE TASK'S TOTAL TIME ``````````````
+sub getTotalTime { 
+    my $self = shift @_;
+
+    my $total_time = $self->{"total_time"};
+
+    return $total_time;
+}
+
+#`````````````````SUBROUTINE TO GET TASK'S ID```````````````````
+sub getPID {
+    my $self = shift @_;
+    my $pid = $self->{"pid"};
+
+    return $pid;
 }
 
 
